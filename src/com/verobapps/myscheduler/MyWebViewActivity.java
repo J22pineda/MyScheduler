e2e1d23b088a6a1c17ba06a73e06a11828aa8af7
Javier Pineda <j22pineda@gmail.com>
2014-03-09 01:59:28 -0800

    Fixed 404 error.

--- src/com/verobapps/myscheduler/MyWebViewActivity.java
+++ src/com/verobapps/myscheduler/MyWebViewActivity.java
@@ -14,7 +14,9 @@ public class MyWebViewActivity extends FragmentActivity {
 
 	public static String SCHEDULE_LINK = "https://enterpriseportal.disney.com/site/dlr/template"
 			+ ".MAXIMIZE/menuitem.427e06f258729e615927f59354276099/?javax.portlet"
-			+ ".tpst=65fa5153808737f60d9cab10b5e26099_ws_MX&javax.portlet.prp_65fa5153808737f60d9cab10b5e26099_viewID=maximized&javax.portlet.prp_65fa5153808737f60d9cab10b5e26099_dsfnavstate=%252FEHHLSM%252FViewCalendar.do%253Bjsessionid%253DD2AB05CE88A7E2DF075DC48F81CD37C9&beanID=1406643234&viewID=maximized&method=loadViewMySchedule&a=1";
+			+ ".tpst=65fa5153808737f60d9cab10b5e26099_ws_MX&javax.portlet" +
+            ".prp_65fa5153808737f60d9cab10b5e26099_viewID=maximized&javax.portlet" +
+            ".prp_65fa5153808737f60d9cab10b5e26099_dsfnavstate=%252FEHHLSMRC%252FViewCalendar.do%253Bjsessionid%253DD2AB05CE88A7E2DF075DC48F81CD37C9&beanID=1406643234&viewID=maximized&method=loadViewMySchedule&a=1";
 
     public static String SCHEDULE_HTML = "";
 
